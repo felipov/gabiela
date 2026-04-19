5361b7520f98ef0b35822bb19a6dac76642a551c
felipo <felipevelosocn@gmail.com>
2026-04-19 14:13:08 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App () {
+  return (
+    <>
+      <header className="header">
+        <img src="/gabiela.jpg" className="gabielaicon"></img>
+      </header>
+    </>
+  )
+}
